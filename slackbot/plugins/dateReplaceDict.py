b72417912025ad1fe3a7a7a81415d4a6460a9f4c
kazuhirofukusima <c01152838a@edu.teu.ac.jp>
2018-04-09 01:09:07 +0900

    issues #7 #3, upload

---
+++ slackbot/plugins/dateReplaceDict.py
@@ -0,0 +1,12 @@
+def getDateDict():
+    dateReplaceDict = {
+        # basic
+        '2017_kihon-a':'2017-weekday',
+        '2017_kihon-b':'2017-saturday',
+        '2018_kihon-a':'weekday',
+        '2018_kihon-b':'saturday',
+        # extraordinary
+        '2018_0305-0405':'20180403.20180405',
+        '2018_0312-0404':'20180402.20180404',
+    }
+    return dateReplaceDict
---
+++ slackbot/plugins/timeTableData.py
@@ -0,0 +1,10 @@
+class TimeTableData:
+    '''
+    引数としてバス時刻表データを受け取り，辞書型に変換し扱う
+    '''
+
+    def __init__(self, ):
+        '''
+        コンストラクタ
+        '''
+        print('this is constructor')
