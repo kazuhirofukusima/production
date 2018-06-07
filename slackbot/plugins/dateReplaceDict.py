c98af9049480aacc2260eb3ed128afa835756438
kazuhirofukusima <c01152838a@edu.teu.ac.jp>
2018-06-08 01:42:23 +0900

    issues #13 完了, DateReplaceDict.pyに文字列の適切な変換処理を追記

--- slackbot/plugins/dateReplaceDict.py
+++ slackbot/plugins/dateReplaceDict.py
@@ -5,8 +5,9 @@ def getDateDict():
         '2017_kihon-b':'2017-saturday',
         '2018_kihon-a':'weekday',
         '2018_kihon-b':'saturday',
-        # extraordinary
+        # temporary
         '2018_0305-0405':'20180403.20180405',
         '2018_0312-0404':'20180402.20180404',
+        '2018_0610-1021':'20180610.20180708.20180930.20181021',
     }
     return dateReplaceDict
