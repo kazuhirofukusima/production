9c74b00ec2a9ac8473b7435a79158e8103178747
kazuhirofukusima <c01152838a@edu.teu.ac.jp>
2018-12-06 00:07:18 +0900

    issue #19 完了．URLをインライン表示に

--- slackbot/plugins/my_mention.py
+++ slackbot/plugins/my_mention.py
@@ -175,7 +175,7 @@ def getSearchResult(topURL, option):
             else:
                 returnMessage = getAppropriateBus(None, busList)
 
-    return returnMessage + '\nurl：' + targetURL
+    return returnMessage + '\n`url：' + targetURL + '`'
 
 
 
